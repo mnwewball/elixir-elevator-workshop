0b92970b75d17c884d349370fe1c3c35b79a8234
McCarthy Newball <mnewball@hugeinc.com>
2017-03-28 22:02:54 -0500

    Removed all workshop code

--- lib/elevator/elevator.ex
+++ lib/elevator/elevator.ex
@@ -1,71 +1,72 @@
 defmodule ElevatorContext do
-  defstruct lowest_floor: 0, highest_floor: 0
+  defstruct lowest_floor: 0, highest_floor: 0, timer: nil
 end
 
 defmodule Elevator do
-  defstruct current: 0, direction: :idle, status: :ok, moving?: false, queue: [], context: %ElevatorContext{}
+  defstruct current: 0, action: :idle, status: :ok, queue: [], context: %ElevatorContext{}
 end
 
-defmodule Elevator.Elevator do
-  @moduledoc """
-  Module for managing state and event management of an individual elevator.
-  """
+# Define the module which will host the elevator's functionality
 
-  @doc """
-  Given the elevator's `initial_state`, assign it a `state_machine` for resolving
-  state transitions and start receiving events.
-  """
-  def start_elevator(initial_state) do
-    update_elevator(initial_state)
-  end
+  # The tick interval for updating the elevator state
 
-  defp update_elevator(state) do
-    IO.puts "In update_elevator"
-    receive do
+  # Define the elevator's bootstrap function
+  
+    # Print some debugging messages
+    
+    # Start the timer
+
+    # Call the update_elevator function
+
+  # Function to update elevator's state
+
+    # Receive messages
+    
       # A clock tick event
-      { :tick } ->
-        state |> state_machine_naive |> update_elevator
-      # A get info event
-      { :get_info, pid } ->
-        { :returned_info, state } |> answer_back_state(pid)
-        state |> update_elevator
-      # The elevator has been requested
-      { :request, floor } ->
-        floor |> enqueue(state) |> update_elevator
-      # dismount gracefully tears down the elevator
-      { :dismount } -> { :ok }
-    end
-  end
+      
+        # Update machine state through a state machine
 
-  @doc """
-  """
-  defp state_machine_naive(:make_stop, state = { floor, direction, _, _, queue }) do
+      # A get info event
+      
+      # The elevator has been requested
+      
+        # Update queue
+
+      # dismount gracefully tears down the elevator
+      
+  # Define a state machine function
 
     # Check if there are any floors left to visit in the queue. If so, move the
     # elevator in the direction of the next floor in the queue. Otherwise, leave 
     # the elevator idle.
-    case queue do
-      [ next | remaining ] when head == floor ->
-        %Elevator{ current: floor, direction: :idle, moving?: false, queue: remaining }
-      [ next | remaining ] ->
-        direction = if floor - head < 0 do :up else :down end
-        %Elevator{ current: floor, direction: direction, queue: remaining }
-      [] ->
-        %Elevator{ current: floor, direction: :idle }
-    end
+
+      # If the next in the queue is the current floor, open the doors. And remove 
+      # the current floor from the queue.
+
+      # If there's more floors in the queue, move towards it. If the doors were 
+      # closed (i.e. the elevator was moving) change the current floor, otherwise
+      # pretend the doors just closed and the elevator started moving.
+
+      # If there's no more floors in the queue, go idle.
+    
+  # Function to start the timer
+  defp start_timer() do
+    { _, timer } = :timer.send_interval @tick_interval, self(), { :tick }
+    timer
   end
 
+  # Function to enqueue a floor request
   defp enqueue(floor, state) do
     # Adds a floor to the queue if it's not already there
-    if Enum.member?(state.queue, floor) do
+    if floor < state.context.lowest_floor or floor > state.context.highest_floor or Enum.member?(state.queue, floor) do
       state
     else
-      %Elevator{ state | queue: queue ++ [ floor ] }
+      %Elevator{ state | queue: state.queue ++ [ floor ] }
     end
   end
 
+  # Answers back a message
   defp answer_back_state(state, pid) do
-    # Answers back a message
     send pid, state
   end
 end
--- test/elevator_test.exs
+++ test/elevator_test.exs
@@ -2,11 +2,69 @@ defmodule ElevatorTest do
   use ExUnit.Case
   doctest Elevator.Elevator
 
-  test "starts an elevator" do
+  setup do
     context = %ElevatorContext{lowest_floor: -2, highest_floor: 8}
-    initial_state = %Elevator{ current: 0, direction: :idle, status: :ok, moving?: false, queue: [], context: context }
-    pid = spawn fn () -> start_elevator(initial_state) end
+    initial_state = %Elevator{ current: 0, action: :idle, status: :ok, queue: [], context: context }
     
+    pid = spawn fn () -> Elevator.Elevator.start_elevator(initial_state) end
+
+    { :ok, initial_state: initial_state, pid: pid }
+  end
+
+  @tag timeout: 30
+  test "starts an elevator", %{ initial_state: initial_state, pid: pid } do
+    
+    state = get_info pid
+
+    assert state == initial_state
+    
+    dismount_elevator pid
+  end
+
+  test "the elevator moves on every clock tick", %{ initial_state: initial_state, pid: pid } do
+
+    request_floors pid, [2]
+
+    # 3.5s would give us enough time for the elevator to move
+    :timer.sleep(2500)
+
+    state = get_info pid
+
+    assert state != initial_state
+    assert state.action != initial_state.action
+    assert state.current > initial_state.current
+
+    dismount_elevator pid
+  end
+
+  test "valid floors get added to the queue upon request", %{ initial_state: initial_state, pid: pid } do
+
+    request_floors pid, [2, 5, -1, 2, 10, 8]
+
+    expected_queue = [2, 5, -1, 8]
+
+    state = get_info pid
+
+    assert state.queue == expected_queue
+
+    dismount_elevator pid
+  end
+
+  defp request_floors(pid, floors) do
+    for floor <- floors do
+      send pid, { :request, floor }
+    end
+  end
+
+  defp get_info(pid) do
+    send pid, { :get_info, self() }
+
+    receive do
+      { :returned_info, state } -> state
+    end
+  end
+
+  defp dismount_elevator(pid) do
     send pid, { :dismount }
   end
 end
